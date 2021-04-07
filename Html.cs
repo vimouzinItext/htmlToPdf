ddc6e9f6cfc2dd86fd1a91aeddf762fecd3d1b5a
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2021-04-07 15:24:00 -0400

    Add files via upload

---
+++ Html.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Html
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream htmlSource = File.Open("input.html", FileMode.Open))
+            using (FileStream pdfDest = File.Open("output.pdf", FileMode.OpenOrCreate))
+            {
+                ConverterProperties converterProperties = new ConverterProperties();
+                HtmlConverter.ConvertToPdf(htmlSource, pdfDest, converterProperties);
+            }
+        }
+    }
+}
